e8cdc77ce1fbca0294c8eeeb4b4dc9f5b0f1327e
Carson Kramer <carsonkra.71755@redlandsschools.net>
2018-09-10 15:25:25 +0000

    hi

---
+++ age.py
@@ -0,0 +1,6 @@
+age=int(input('What is your age?: '))
+if age > 17:
+    print('Wow, you can vote!')
+else:
+    print("Oh no, you can't vote yet.")
+    
---
+++ first.py
@@ -0,0 +1,3 @@
+number=int(input('Enter a number: '))
+print(str('If I add 6 to that, I get...'))
+print(number+6)
